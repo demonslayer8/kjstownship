1ead2b46f9e5901bd90e524fa7b674a04dd0f796
demonslayer8 <aalokkumarroy12@gmail.com>
2026-04-25 15:34:42 +0530

    added root route

--- Server.js
+++ Server.js
@@ -55,3 +55,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("KJS Township backend is running 🚀");
+});
